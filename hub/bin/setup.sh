dc2bd4988024f74009af02c285dc2504f3e4fe7f
Owen W. Taylor <otaylor@fishsoup.net>
2017-08-25 01:26:48 -0400

    Use a workstation-relevant address for kojifiles for the web UI

--- hub/bin/setup.sh
+++ hub/bin/setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -xeuo pipefail
 
+WORKSTATION_IP=$(/sbin/ip route | awk '/default/ { print $3 }')
+sed -i "s/WORKSTATION_IP/${WORKSTATION_IP}/" /etc/kojiweb/web.conf
+
 # Use kojiadmin user by default
 mkdir -p /root/.koji
 ln -fs /opt/koji-clients/kojiadmin/config /root/.koji/config
